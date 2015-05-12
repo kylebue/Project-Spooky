98e21391e813f6012109bdb8e38c861cda6973d5
Kyle Bue <kylebue9@gmail.com>
2015-05-11 17:00:00 -0700

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player = null;
+	public Vector3 playerPos = new Vector3 (0, 0, 0);
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		playerPos = player.gameObject.transform.position;
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/ChestScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestScript : MonoBehaviour 
+{
+	public GameObject player;
+	public bool isOpened = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.E) && Vector3.Distance(player.transform.position, transform.position) <= 1.5f && !isOpened)
+		{
+			Debug.Log("Opened Chest!");
+			isOpened = true;
+		}
+	}
+}
