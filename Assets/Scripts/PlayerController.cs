2828e06618bbe51b181731ed241a333d4f8f430e
Kyle Bue <kylebue9@gmail.com>
2015-05-18 14:25:08 -0700

    Made the movements side-scrolling only as well as added "slowdown" effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,18 @@ using Chronos;
 public class PlayerController : BaseBehaviour 
 {
 	// Character movement speed values
-	private float walkSpeed = 0.12f;
-	private float runSpeed = 0.18f;
-	private float diagonalWalkSpeed = 0f;
-	private float diagonalRunSpeed = 0f;
+	private float walkSpeed = 0.06f;
+	private float runSpeed = 0.1f;
 	private Vector3 pos;
 	private Quaternion rot;
 
 	protected bool canMove = true;
 
-	public LocalClock playerClock;
-
+	protected float speedModifier = 0.0f;
 
 	// Use this for initialization
 	void Start () 
 	{
-		playerClock = GetComponent<LocalClock> ();
 		rot = transform.rotation;
 	}
 
@@ -36,135 +32,36 @@ public class PlayerController : BaseBehaviour
 	// How the player moves the avatar
 	private void MovementInputs()
 	{
+		speedModifier = Timekeeper.instance.Clock("Player").localTimeScale;
 		transform.rotation = rot;
 		pos = transform.position;
-		diagonalWalkSpeed = walkSpeed * 2;
-		diagonalRunSpeed = runSpeed * 2;
 
-		// Left, Right, Up, Down
-		if (Input.GetKey(KeyCode.W))
+		// Left, Right
+		if (Input.GetKey(KeyCode.D))
 		{
-			pos.y += walkSpeed;
-			transform.position = pos;
-		}
-		else if (Input.GetKey(KeyCode.S))
-		{
-			pos.y -= walkSpeed;
-			transform.position = pos;
-		}
-		else if (Input.GetKey(KeyCode.D))
-		{
-			pos.x += walkSpeed;
+			pos.x += walkSpeed * speedModifier;
 			transform.position = pos;
 		}
 		else if (Input.GetKey(KeyCode.A))
 		{
-			pos.x -= walkSpeed;
+			pos.x -= walkSpeed * speedModifier;
 			transform.position = pos;
-		}
-
-		// Diagonals
-		if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
-		{
-			walkSpeed = 0.06f;
-			pos.y += walkSpeed;
-			pos.x += diagonalWalkSpeed;
-			transform.position = pos;
-		}
-		else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
-		{
-			walkSpeed = 0.06f;
-			pos.y += walkSpeed;
-			pos.x -= diagonalWalkSpeed;
-			transform.position = pos;
-		}
-		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
-		{
-			walkSpeed = 0.06f;
-			pos.y -= walkSpeed;
-			pos.x += diagonalWalkSpeed;
-			transform.position = pos;
-		}
-		else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
-		{
-			walkSpeed = 0.06f;
-			pos.y -= walkSpeed;
-			pos.x -= diagonalWalkSpeed;
-			transform.position = pos;
-		}
-		else
-		{
-			walkSpeed = 0.12f;
 		}
 
 		// Running
 		if (Input.GetKey(KeyCode.LeftShift))
 		{
-			// Left, Right, Up, Down
-			if (Input.GetKey(KeyCode.W))
+			// Left, Right
+			if (Input.GetKey(KeyCode.D))
 			{
-				pos.y += runSpeed;
-				transform.position = pos;
-			}
-			else if (Input.GetKey(KeyCode.S))
-			{
-				pos.y -= runSpeed;
-				transform.position = pos;
-			}
-			else if (Input.GetKey(KeyCode.D))
-			{
-				pos.x += runSpeed;
+				pos.x += runSpeed * speedModifier;
 				transform.position = pos;
 			}
 			else if (Input.GetKey(KeyCode.A))
 			{
-				pos.x -= runSpeed;
+				pos.x -= runSpeed * speedModifier;
 				transform.position = pos;
 			}
-			
-			// Diagonals
-			if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
-			{
-				runSpeed = 0.09f;
-				pos.y += runSpeed;
-				pos.x += diagonalRunSpeed;
-				transform.position = pos;
-			}
-			else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
-			{
-				runSpeed = 0.09f;
-				pos.y += runSpeed;
-				pos.x -= diagonalRunSpeed;
-				transform.position = pos;
-			}
-			else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
-			{
-				runSpeed = 0.09f;
-				pos.y -= runSpeed;
-				pos.x += diagonalRunSpeed;
-				transform.position = pos;
-			}
-			else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
-			{
-				runSpeed = 0.09f;
-				pos.y -= runSpeed;
-				pos.x -= diagonalRunSpeed;
-				transform.position = pos;
-			}
-			else
-			{
-				runSpeed = 0.18f;
-			}
-		}
-
-		// Time Control Test Movement
-		if (Input.GetKey(KeyCode.LeftControl))
-		{
-			playerClock.localTimeScale = 0.5f;
-		}
-		else
-		{
-			playerClock.localTimeScale = 1;
 		}
 	}
 
---
+++ Assets/Scripts/TimeControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Chronos;
+
+public class TimeControl : MonoBehaviour 
+{	
+	// Update is called once per frame
+	void Update () 
+	{
+		Clock playerClock = Timekeeper.instance.Clock ("Player");
+
+		if (Input.GetKey(KeyCode.LeftControl))
+		{
+			playerClock.localTimeScale = 0.5f;
+		}
+		else
+		{
+			playerClock.localTimeScale = 1.0f;
+		}
+	}
+}
